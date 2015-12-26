8ef65b84c70b23f40aac1725884bbf14f1cc5586
Mico Piira <mico.piira@gmail.com>
2015-12-26 17:50:19 +0200

    Convert title-* and top-* to xpm

--- xfwm4/convert_to_xpm.sh
+++ xfwm4/convert_to_xpm.sh
@@ -6,3 +6,9 @@ do
     echo "File: $f"
     convert "$f" -resize 12x12^ -background transparent -gravity center -extent 20x24 "${f%%.*}.xpm"
 done
+
+for f in title-*.png top-*.png;
+do
+    echo "File: $f"
+    convert "$f" -background transparent "${f%%.*}.xpm"
+done
