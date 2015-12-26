9a6ea7e6c04630335052bac871d14bf04eb05ede
Mico Piira <mico.piira@gmail.com>
2015-12-26 17:21:04 +0200

    Change circle sizes to 12px

--- xfwm4/convert_to_xpm.sh
+++ xfwm4/convert_to_xpm.sh
@@ -4,5 +4,5 @@
 for f in close-*.png hide-*.png maximize-*.png;
 do
     echo "File: $f"
-    convert "$f" -resize 13x13^ -gravity center -extent 20x24 "${f%%.*}.xpm"
+    convert "$f" -resize 12x12^ -gravity center -extent 20x24 "${f%%.*}.xpm"
 done
